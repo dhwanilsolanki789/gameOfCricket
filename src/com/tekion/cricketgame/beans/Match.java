274971b824d2fb380e03c7bad648b716da3c48ea
dhwanilsolanki789 <dhwanilsolanki789@gmail.com>
2023-02-01 20:04:33 +0530

    Restructured & delegated code flow and fixed bug

--- src/com/tekion/cricketgame/beans/Match.java
+++ src/com/tekion/cricketgame/beans/Match.java
@@ -1,15 +1,18 @@
 package src.com.tekion.cricketgame.beans;
 
-import java.util.SortedMap;
-
 public class Match {
     private final Team t1;
     private final Team t2;
     private final int matchOvers;
+    private final int maxWickets;
+    private int oversCompleted;
+    private int ballsBowled;
+    private int matchTarget;
 
     public Match(int overs, String team1Name, String team2Name) {
-        t1 = new Team(team2Name);
+        t1 = new Team(team1Name);
         t2 = new Team(team2Name);
+        this.maxWickets = 5;
         matchOvers = overs;
     }
 
@@ -23,6 +26,76 @@ public class Match {
 
     public int getMatchOvers() {
         return matchOvers;
+    }
+
+    public int playInning(int inningNo,Team battingTeam, Team bowlingTeam){
+        String team1 = battingTeam.getTeamName();
+        String team2 = bowlingTeam.getTeamName();
+
+        //Inning setup
+        int overs = this.matchOvers;
+        int currScore = 0;
+        int currWickets = 0;
+        boolean inningEnded = false;
+
+        int currOver, currBall = 0;
+        for (currOver = 1; currOver <= overs; currOver++) {
+            currBall = 0;
+            for (; currBall < 6; currBall++) {
+                //Storing ball outcome
+                char ballScore = this.bowlBall();
+                //Check for wicket or runs
+                if (ballScore != 'W') {
+                    currScore += (ballScore - '0');
+                    //Second inning check for target reach
+                    if(inningNo == 2){
+                        if(currScore >= matchTarget) {
+                            System.out.println("Innings ended!");
+                            inningEnded = true;
+                            break;
+                        }
+                    }
+                } else {
+                    //Wicket logged
+                    System.out.println(team1 + " are " + (++currWickets) + " down.");
+
+                    //End inning if all wickets down
+                    if (currWickets == maxWickets) {
+                        System.out.println("Innings ended!");
+                        inningEnded = true;
+                        break;
+                    }
+                }
+            }
+            if (inningEnded) {
+                break;
+            }
+            //Over logged
+            System.out.println(currOver + " over ends!");
+        }
+        //Store inning stats
+        this.oversCompleted = currOver;
+        this.ballsBowled = currBall;
+        battingTeam.setTeamScore(currScore);
+        battingTeam.setWicketsFell(currWickets);
+        //Print inning stats
+        System.out.println(team1 + " scores " + currScore + " runs.");
+        System.out.println("!----------------------------------------!");
+        return currScore;
+    }
+
+    public void printResults(int inningScore, Team battingTeam, Team bowlingTeam) {
+        //Print match results
+        if (inningScore >=matchTarget) {
+            System.out.println(battingTeam.getTeamName() + " won by " + (5 - battingTeam.getWicketsFell()) + " wickets.");
+        }
+        if (inningScore <matchTarget - 1){
+            System.out.println(bowlingTeam.getTeamName() + " won by " + (matchTarget - inningScore - 1) + " runs.");
+        }
+        // Check if match tied or print game results
+        if (inningScore == matchTarget - 1) {
+            System.out.println("Match tied!");
+        }
     }
 
     public Team[] flipCoin() {
@@ -63,4 +136,13 @@ public class Match {
             return 'W';
         }
     }
+
+    public int getMatchTarget() {
+        return matchTarget;
+    }
+
+    public void setMatchTarget(int matchTarget) {
+        this.matchTarget = matchTarget;
+    }
+
 }
--- src/com/tekion/cricketgame/controller/MatchController.java
+++ src/com/tekion/cricketgame/controller/MatchController.java
@@ -3,11 +3,12 @@ package src.com.tekion.cricketgame.controller;
 import src.com.tekion.cricketgame.beans.Match;
 import src.com.tekion.cricketgame.beans.Team;
 
-import java.util.Scanner;
-
 public class MatchController {
     private final Match match;
     private final int maxWickets;
+    private int inningScore;
+    private Team battingTeam;
+    private Team bowlingTeam;
 
     public MatchController(int overs, String team1Name, String team2Name) {
         this.match = new Match(overs,team1Name,team2Name);
@@ -16,130 +17,31 @@ public class MatchController {
 
 
     public void startGame() {
-
-        System.out.println("Game Started!");
+        setupGame();
+        inningScore = match.playInning(1,battingTeam,bowlingTeam);
+        changeInning(inningScore);
+        inningScore = match.playInning(2,battingTeam, bowlingTeam);
+        match.printResults(inningScore,battingTeam,bowlingTeam);
+    }
+    public void setupGame() {
+        System.out.println("Game Starts!");
         // Toss
         Team[] toss = match.flipCoin();
         System.out.println("Coin flipped!");
-
         // batting & bowling team assigned
-        Team battingTeam = toss[0];
-        Team bowlingTeam = toss[1];
-        String team1 = battingTeam.getTeamName();
-        String team2 = bowlingTeam.getTeamName();
-        System.out.println(team1 + " bats first!");
+        battingTeam = toss[0];
+        bowlingTeam = toss[1];
+        //Inning commencement
+        System.out.println(battingTeam.getTeamName() + " bats first!");
+        System.out.println("!----------------------------------------!");
+    }
 
-        //Match & first inning setup
-        int overs = match.getMatchOvers();
-        int currScore = 0;
-        int currWickets = 0;
-        boolean inningEnded = false;
-
-        //First Inning starts
-        for (int over = 1; over <= overs; over++) {
-            int ball = 0;
-            for (; ball < 6; ball++) {
-                //Storing ball outcome
-                char ballScore = match.bowlBall();
-
-                //Check for wicket or runs
-                if (ballScore != 'W') {
-                    currScore += (ballScore - '0');
-                } else {
-                    //Wicket logged
-                    System.out.println(team1 + " are " + (++currWickets) + " down.");
-
-                    //End inning if all wickets down
-                    if (currWickets == maxWickets) {
-                        System.out.println("First innings ended!");
-                        inningEnded = true;
-                        break;
-                    }
-                }
-            }
-            if (inningEnded) {
-                break;
-            }
-
-            //Over logged
-            System.out.println(over + " over ends!");
-        }
-
-        //Storing first inning stats
-        battingTeam.setTeamScore(currScore);
-        battingTeam.setWicketsFell(currWickets);
-        //Printing inning stats
-        System.out.println(team1 + " scores " + currScore + " runs.");
-
-        //Second inning setup
-        int target = currScore + 1;
+    public void changeInning(int inningScore) {
+        match.setMatchTarget(inningScore+1);
         Team temp = battingTeam;     //bowling & batting team reassigned
         battingTeam = bowlingTeam;
         bowlingTeam = temp;
-        System.out.println(target + " is the target for " + team2);
-        currScore = 0;
-        currWickets = 0;
-        inningEnded = false;
-        int over, ball = 0;
-
-        // Second inning starts
-        for (over = 1; over <= overs; over++) {
-            ball = 0;
-            for (; ball < 6; ball++) {
-                //Storing ball outcome
-                char ballScore = match.bowlBall();
-
-                //Check for wicket or runs
-                if (ballScore != 'W') {
-                    currScore += (ballScore - '0');
-
-                    // Check for target achieved
-                    if (currScore >= target) {
-
-                        //Printing game results
-                        System.out.println(team2 + " scores " + currScore + " runs.");
-                        System.out.println("Second innings ended!");
-                        System.out.println(team2 + " won by " + (5 - battingTeam.getWicketsFell()) + " wickets.");
-                        inningEnded = true;
-                        break;
-                    }
-                } else {
-                    //Wicket logged
-                    System.out.println(team2 + " are " + (++currWickets) + " down.");
-
-                    //End inning if all wickets down
-                    if (currWickets == maxWickets) {
-                        //Printing inning stats
-                        System.out.println(team2 + " scores " + currScore + " runs.");
-                        System.out.println("Second innings ended!");
-                        if (currScore < target - 1) {
-                            //Printing game results
-                            System.out.println(team1 + " won by " + (target - currScore - 1) + " runs.");
-                        }
-                        inningEnded = true;
-                        break;
-                    }
-                }
-            }
-            if (inningEnded) {
-                break;
-            }
-            //Over logged
-            System.out.println(over + " over ends!");
-        }
-
-        // Storing second innings stats
-        battingTeam.setTeamScore(currScore);
-        battingTeam.setWicketsFell(currWickets);
-
-//      System.out.println(i + " overs and " + j + " balls");
-
-        // Check if match tied or print game results
-        if (currScore == target - 1) {
-            System.out.println("Match tied!");
-        } else if (currScore < target && ((over == overs + 1) && (ball == 6))) {
-            System.out.println(team2 + " scores " + currScore + " runs.");
-            System.out.println(team1 + " won by " + (target - currScore - 1) + " runs.");
-        }
+        System.out.println((inningScore+1) + " is the target for " + battingTeam.getTeamName());
+        System.out.println("!----------------------------------------!");
     }
 }
